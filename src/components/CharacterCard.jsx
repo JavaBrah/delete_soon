cb382ca4d2edfd81df77ce2c139f7225069d9599
Kegan <kegcliffjohnson@email.com>
2025-07-11 09:08:16 -0400

    character card added

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+
+export default function CharacterCard(){
+    return(
+        <>
+            <Link to="Characters">
+                <div>
+
+                </div>
+            </Link>
+        </>
+    );
+}
